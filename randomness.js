c4cbdd74bce3c300b965bc05b3629cba64f94f8b
mboyle253 <mboyle253@gmail.com>
2017-05-14 16:07:32 -0700

    Update randomness.js

--- randomness.js
+++ randomness.js
@@ -48,3 +48,88 @@ function makeArrayConsecutive2(statues) {
     
     return count;
 }
+
+
+function almostIncreasingSequence1(sequence) {
+  var checkArray = function(ar){
+    if (ar.length === 2 && ar[0] === ar[1]) return true;
+
+    for (var i=1;i<ar.length;i++){
+      if (ar[i-1] >= ar[i]){
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  var success = checkArray(sequence);
+
+  if (!success) {
+    var s1 = sequence.slice();
+    for (var i = 1; i < sequence.length; i++) {
+      if (sequence[i-1] > sequence[i]){
+        s1.splice(i-1, 1);
+        success = checkArray(s1);
+        break;
+      }
+    }
+
+    if (!success){
+      s1 = sequence.slice();
+      for (var i = 1; i < sequence.length; i++) {
+        if (sequence[i-1] > sequence[i]){
+          s1.splice(i, 1)
+          success = checkArray(s1);
+          break;
+        }
+      }
+    }
+  }
+
+  return success;
+}
+
+
+function matrixElementsSum(matrix) {
+  var tally = [];
+
+  for (var i=0;i<matrix.length;i++){
+    var row = matrix[i];
+    var above = i === 0 ? false : matrix[i-1]
+    for (var x=0;x<row.length;x++){
+      if (row[x] > 0 ){
+        if (!above || above[x] > 0){
+          tally.push(row[x]);
+        }
+
+        if (above && above[x] === 0){
+          row[x] = 0;
+        }
+      }
+    }
+  }
+
+  var sum = 0;
+  for (var y=0;y<tally.length;y++){
+    sum = sum + tally[y];
+  }
+
+  return sum;
+}
+
+function commonCharacterCount(s1, s2) {
+  var s1_ = s1.split('');
+  var s2_ = s2.split('');
+  var count = 0;
+  s1_.some(function (v) {
+    var i = s2_.indexOf(v);
+    if (i >= 0){
+      s2_.splice(i, 1);
+      count = count+1;
+    }
+  });
+
+  return count;
+}
+
