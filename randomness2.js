4585be781851fa50280d57ba0fbd27a60e70db78
mboyle253 <mboyle253@gmail.com>
2017-05-15 20:22:46 -0700

    Create randomness2.js
    More code challenges

---
+++ randomness2.js
@@ -0,0 +1,47 @@
+function alternatingSums(a) {
+  var odds = 0, evens = 0;
+  a.filter(function(v, idx){
+    Math.abs(idx % 2) == 1 ? odds += v : evens += v;
+  });
+
+  return [evens, odds]
+}
+
+function addBorder(picture) {
+  picture.map(function(v, idx){
+    picture[idx] = '*' + v + '*';
+  });
+
+  var frame = '*'.repeat(picture[0].length);
+
+  picture.unshift(frame);
+  picture.push(frame);
+
+  return picture;
+}
+
+function areSimilar(a, b) {
+  var mismatch = [];
+
+  a.map(function(v, i){
+    if (v != b[i]){
+      mismatch.push(i);
+    }
+  });
+
+  return (mismatch.length === 0 ||
+    (mismatch.length === 2 && a[mismatch[0]] === b[mismatch[1]] && a[mismatch[1]] === b[mismatch[0]]));
+
+}
+
+function arrayChange(inputArray) {
+  var moves = 0;
+  inputArray.map(function(v, idx){
+    if (idx > 0 && inputArray[idx-1] >= inputArray[idx]){
+      var t = inputArray[idx-1] + 1;
+      moves += (t-inputArray[idx]);
+      inputArray[idx] = t;
+    }
+  });
+  return moves;
+}
