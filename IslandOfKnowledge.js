8bd973c9d5c6c8697bb8e47cada73b3a38a03647
mboyle253 <mboyle253@gmail.com>
2017-05-16 17:42:33 -0700

    Update IslandOfKnowledge.js

--- IslandOfKnowledge.js
+++ IslandOfKnowledge.js
@@ -2,3 +2,26 @@ function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
   return ((yourLeft === friendsLeft && yourRight === friendsRight) ||
   (yourLeft === friendsRight && yourRight === friendsLeft))
 }
+
+function arrayMaximalAdjacentDifference(inputArray) {
+
+  var max = 0;
+  inputArray.map(function(v, idx, s){
+    if (idx != 0){
+      var v1 = Math.abs(v - s[idx-1]);
+      max = v1 > max ? v1 : max;
+    }
+   });
+
+  return max;
+}
+
+function isIPv4Address(inputString) {
+  var ar = inputString.split('.');
+  if (ar.length != 4) return false;
+
+  for (var i=0; i<ar.length; i++){
+    if (!new RegExp("\\b(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\\b").test(ar[i])) return false;
+  }
+  return true;
+}
