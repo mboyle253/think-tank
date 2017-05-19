2c444647d3bf44d00211f1493cdb55f7f7b925e5
mboyle253 <mboyle253@gmail.com>
2017-05-19 08:47:53 -0700

    Update ThroughTheFog.js

--- ThroughTheFog.js
+++ ThroughTheFog.js
@@ -1,7 +1,15 @@
+function circleOfNumbers(n, firstNumber) {
+  var div = n/2;
+  return firstNumber < div ? firstNumber+div : firstNumber-div;
+}
+
 function depositProfit(deposit, rate, threshold) {
   function calc(d, r, t, c){
-    var balance = (d*(r/100)) + d;
-    return balance >= t ? ++c : calc(balance, r, t, ++c);
+    return (d>=t) ? c : calc((d*(r/100)) + d, r, t, ++c);
   }
   return calc(deposit, rate, threshold, 0);
+}
+
+function absoluteValuesSumMinimization(a) {
+  return a[Math.ceil(a.length/2)-1];
 }
