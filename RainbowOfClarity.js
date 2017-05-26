a3203abc25675774e6eabc2e1d2dd4e79ec0d872
mboyle253 <mboyle253@gmail.com>
2017-05-25 21:42:34 -0700

    Update RainbowOfClarity.js

--- RainbowOfClarity.js
+++ RainbowOfClarity.js
@@ -17,3 +17,17 @@ function lineEncoding(s) {
   return str;
 }
 
+function chessKnight(cell) {
+  var coord = cell.split('');
+
+  var x = coord[0].toUpperCase().charCodeAt(0), y=parseInt(coord[1]), countY= [], countX=[];
+  var a='A'.charCodeAt(0), h='H'.charCodeAt(0);
+  for (var i=1;i<=2;i++){
+    if (y+i <= 8) countY.push(y+i);
+    if (y-i >= 1) countY.push(y-i);
+    if (x+i <= h) countX.push(x+i);
+    if (x-i >= a) countX.push(x-i);
+  }
+
+  return  Math.floor((countY.length * countX.length)/2);
+}
