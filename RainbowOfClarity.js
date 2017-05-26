222e3f8ceb11d8a02412b0ee665f4902e5be6f83
mboyle253 <mboyle253@gmail.com>
2017-05-25 21:32:13 -0700

    Update RainbowOfClarity.js

--- RainbowOfClarity.js
+++ RainbowOfClarity.js
@@ -1,3 +1,19 @@
 function isDigit(symbol) {
     return !Number.isNaN(parseInt(symbol));
 }
+
+function lineEncoding(s) {
+  var arr = s.split('');
+  var holder = [], str='';
+  while (arr.length > 0){
+    holder.push(arr.shift());
+
+    if (holder[0] != arr[0]){
+      str = holder.length > 1 ? str + holder.length + holder[0] : str + holder[0];
+      holder = [];
+    }
+  }
+
+  return str;
+}
+
