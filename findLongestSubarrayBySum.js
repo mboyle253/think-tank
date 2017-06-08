189760899b3c44ccabf9a3ceded596d3729b72c0
mboyle253 <mboyle253@gmail.com>
2017-06-07 19:42:19 -0700

    Create findLongestSubarrayBySum.js

--- findLongestSubarrayBySum.js
+++ findLongestSubarrayBySum.js
@@ -1,4 +1,14 @@
 function findLongestSubarrayBySum(s, arr) {
+  /*
+  You have an unsorted array arr of non-negative integers and a number s. 
+  Find a longest contiguous subarray in arr that has a sum equal to s. 
+  Return two integers that represent its inclusive bounds. 
+  If there are several possible answers, return the one with the smallest left bound. 
+  If there are no answers, return [-1].
+
+  Your answer should be 1-based, meaning that the first position of the array is 1 instead of 0.
+  /*
+  
   /** Build the prefixes. While the prefixes are building, check if the subarray exists from previous entries.
 
   var pfxs = [], sum = 0, c = [-1], length=0;;
